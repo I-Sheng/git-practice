6bd4db1d7cafccb90ef5a268488bf8a51739d31b
isheng <110703011@g.nccu.edu.tw>
2024-09-21 00:34:50 +0800

    feat: add unit test in main.js for the Stack object in stack.js

--- week-02/main.js
+++ week-02/main.js
@@ -3,10 +3,18 @@
 import Stack from './stack.js';
 
 let stack = new Stack();
-stack.print();
-
-stack.push(5);
-stack.push(8);
-stack.print();
+console.assert(stack.isEmpty(), "The init stack is not empty, there must be something wrong."); //First, check whether the stack is empty
+stack.print(); //Print out the empty stack for checking
+for (let i = 0; i < 10; i++) {
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    stack.push(randomNumber);
+} //Add 10 random number for testing
+stack.print(); //Print out stack
+console.log(stack.peek()); //Peek the last one
+console.log(`This pop() output must be the same as the previous one: ${stack.pop()}`); //Check the peek and pop function work well
+console.log(`The size of stack is ${stack.size()}`); //The size here must be 9
+stack.clear();
+console.assert(stack.isEmpty(), "Something went wrong, it executed clear function just now!"); // Check clear function works well
+stack.print() //Finally print the empty stack out
 
 // TODO: 應該還要做哪些測試，以驗證自己開發的 stack 是沒有問題的？
