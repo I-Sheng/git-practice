4b4b5f8f1295f74b1b78d8733677a4300216e99d
isheng <110703011@g.nccu.edu.tw>
2024-09-26 21:10:27 +0800

    feat: add iterative_fib in week-02/fib.js

--- week-02/fib.js
+++ week-02/fib.js
@@ -1,14 +1,35 @@
-function fib(n) {
-    if(n == 0) {
-        return 0;
+//recursive_fib
+function recursive_fib(n) {
+    if(n <= 1) {
+        return n;
     }
-    else if(n == 1){
-        return 1;
-    }
-    return fib(n-1) + fib(n-2);
+    return recursive_fib(n-1) + recursive_fib(n-2);
 }
 
-console.log(fib(0)); // 0
-console.log(fib(1)); // 1
-console.log(fib(5)); // 5
-console.log(fib(10)); // 55
+//iterative_fib
+function iterative_fib(n) {
+    if(n <= 1) {
+        return n;
+    }
+
+    let a = 0, b = 1, c;
+
+    for(let i = 2; i <= n; ++i) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
+}
+
+//testing
+console.log(recursive_fib(0)); // 0
+console.log(recursive_fib(1)); // 1
+console.log(recursive_fib(5)); // 5
+console.log(recursive_fib(10)); // 55
+//
+
+console.log(iterative_fib(0)); // 0
+console.log(iterative_fib(1)); // 1
+console.log(iterative_fib(5)); // 5
+console.log(iterative_fib(10)); // 55
